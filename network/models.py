6f7c378711890c190a02f2f876127c2092f75d6f
Pr4c0w1ty <miloszpracowity@wp.pl>
2024-08-07 17:34:29 +0200

    models.py migrated

--- network/models.py
+++ network/models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
-    following = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="following")
-    followers = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="followers")
-    pass
+    # Users that the user is following
+    following = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="following_of_user")
+    # Users that follow the user
+    followers = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="followers_of_user")
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
