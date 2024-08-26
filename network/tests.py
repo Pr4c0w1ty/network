d0bc7a5f066eb85a21a093a35ce4a8d53f6c2de3
Pr4c0w1ty <miloszpracowity@wp.pl>
2024-08-27 01:51:46 +0200

    added test

--- network/tests.py
+++ network/tests.py
@@ -1,15 +1,48 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.contrib.auth import get_user_model
 from .models import Post, Like, Follow
 
-# test the views
+
+
 class NetworkTestCase(TestCase):
     def setUp(self):
-        self.user1 = get_user_model().objects.create_user(username="user1", password="password")
-        self.user2 = get_user_model().objects.create_user(username="user2", password="password")
-        self.post1 = Post.objects.create(user=self.user1, content="post1")
-        self.post2 = Post.objects.create(user=self.user2, content="post2")
-        self.like1 = Like.objects.create(user=self.user1, post=self.post2)
-        self.follow1 = Follow.objects.create(user=self.user1, user_follower=self.user2)
-        
+        self.user = get_user_model().objects.create_user(username="testuser", password="testpassword")
+        self.user2 = get_user_model().objects.create_user(username="testuser2", password="testpassword")
+        self.post = Post.objects.create(user=self.user, content="Test content")
+        self.like = Like.objects.create(user=self.user, post=self.post)
+        self.follow = Follow.objects.create(user=self.user, user_follower=self.user2)
+        # Set up the client
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_post(self):
+        self.assertEqual(self.post.content, "Test content")
+        self.assertEqual(self.post.user, self.user)
+
+    def test_like(self):
+        self.assertEqual(self.like.user, self.user)
+        self.assertEqual(self.like.post, self.post)
+
+    def test_index(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "network/index.html")
+
+    def test_remove_like(self):
+        self.client.force_login(self.user)
+        response = self.client.post(f"/remove_like/{self.post.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["message"], "Like removed.")
+
+    def test_add_like(self):
+        self.client.force_login(self.user)
+        response = self.client.post(f"/add_like/{self.post.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["message"], "Like added.")
+
+    def test_new_post(self):
+        self.client.force_login(self.user)
+        response = self.client.post("/new_post", {"content": "New content"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().content, "New content")
 
